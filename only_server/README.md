2efd60df2770ce0f0449f05c1fdf03dd331c3658
richaaccion <richa.joshi@accionlabs.com>
2018-08-14 13:23:35 +0530

    example 1 created

---
+++ only_server/README.md
@@ -0,0 +1,4 @@
+This example contains following:
+
+1) node server using fastify framework : http://0.0.0.0:3002/
+2) one endpoint as root
--- only_server/server.js
+++ only_server/server.js
@@ -3,20 +3,18 @@ const fastify = require("fastify")({
 });
 
 fastify.get("/", (req, res) => {
-	console.log("REgULAR LOG CONTROLLER:: ");
 	res.send({hello: "world"});
 });
 
 const startServer = async () => {
-	fastify.listen(3002, (err, address) => {
+	fastify.listen(3002, "0.0.0.0", (err, address) => {
 		if (err) {
 			console.log("ERROR HERE: ", err);
 			fastify.log.error("errror here> ", err);
-			// throw err;
+			process.exit(1);
 		} else {
 			console.log("REGULAR LOG HERE");
 			fastify.log.info(`server started at ${address}`);	
-			process.exit(1);
 		}
 	});	
 }
